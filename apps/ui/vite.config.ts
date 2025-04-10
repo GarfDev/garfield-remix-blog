9135763c5343ed3ead92f7e7b1e540ecd94966a6
Garfield <gnguyengia@pay.com.au>
2025-04-11 01:04:21 +0700

    feature: add transcript store

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -1,43 +1,12 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { remixPWA } from '@remix-pwa/dev';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// @ts-ignore
-declare module '@remix-run/node' {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    // Custom plugin resolves warning - Failed to resolve "remix:manifest" from /Users/... An id should be written. Did you pass a URL?
-    {
-      name: 'remix-manifest-resolver',
-      resolveId(id) {
-        if (id === 'remix:manifest') {
-          return id;
-        }
-      },
-      // Optional: warning is suppressed without this hook
-      // Provides an empty object for 'remix:manifest' if HMR triggers, but HMR remains non-functional
-      load(id) {
-        if (id === 'remix:manifest') {
-          return 'export default {}';
-        }
-      },
-    },
-    tsconfigPaths(),
-    remixPWA(),
+    //@ts-expect-error: Legacy plugin
+    react(),
+    tailwindcss()
   ],
-});
+})
